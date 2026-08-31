e09ceedba9506c664c074a16c6534f69fda1ce51
Acer <wadimsergeew190405@gmail.com>
2026-08-31 10:23:55 +0300

    Update wrangler configuration

--- functions/api/[[path]].ts
+++ functions/api/[[path]].ts
@@ -1,29 +1,11 @@
-const API_ORIGIN = 'https://serviceai-api.wadimsergeew190405.workers.dev'
+interface Env {
+  API: Fetcher
+}
 
-export const onRequest: PagesFunction = async (context) => {
-  const url = new URL(context.request.url)
-  const target = `${API_ORIGIN}${url.pathname}${url.search}`
-
-  const headers = new Headers(context.request.headers)
-  headers.delete('host')
-
-  const init: RequestInit = {
-    method: context.request.method,
-    headers,
+export const onRequest: PagesFunction<Env> = async (context) => {
+  if (!context.env.API) {
+    return new Response('API service binding is not configured', { status: 502 })
   }
 
-  if (context.request.method !== 'GET' && context.request.method !== 'HEAD') {
-    init.body = context.request.body
-  }
-
-  const response = await fetch(target, init)
-  const responseHeaders = new Headers(response.headers)
-  responseHeaders.set('Access-Control-Allow-Origin', url.origin)
-  responseHeaders.set('Access-Control-Allow-Credentials', 'true')
-
-  return new Response(response.body, {
-    status: response.status,
-    statusText: response.statusText,
-    headers: responseHeaders,
-  })
+  return context.env.API.fetch(context.request)
 }
